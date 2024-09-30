eabdaf36937bb3523ba63c94d3931743efa0a3fe
Christian Bustos <christianbus37@gmail.com>
2024-09-30 18:22:40 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // SELECCIONAR LOS ELEMENTOS DE LA INTERFAZ
+
+  const inputEmail = document.querySelector("#email");
+  const inputAsunto = document.querySelector("#asunto");
+  const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
+
+  //   Asignar eventos
+  inputEmail.addEventListener("blur", validar);
+
+  inputAsunto.addEventListener("blur", validar);
+
+  inputMensaje.addEventListener("blur", validar);
+
+  function validar(e) {
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(
+        `El campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
+      return;
+    }
+
+    limpiarAlerta(e.target.parentElement);
+
+    console.log("Despues del IF");
+  }
+
+  function mostrarAlerta(mensaje, referencia) {
+    // Comrpueba si ya existe una alerta
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove();
+    }
+    // Generar alerta en HTML
+    const error = document.createElement("P");
+    error.textContent = mensaje;
+    error.classList.add("bg-red-600");
+
+    // Inyectar el error al formulario
+    referencia.appendChild(error);
+  }
+});
+
+function limpiarAlerta(referencia) {
+  const alerta = referencia.querySelector(".bg-red-600");
+  if (alerta) {
+    alerta.remove();
+  }
+}
